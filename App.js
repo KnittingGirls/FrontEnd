4a46031a7748e23484dde0bb157727a4f6bb62a7
YJin33 <ujin3261@gmail.com>
2024-11-14 02:12:56 +0900

    초기설정&이미지 업로드

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './Pages/Home';
+import SelectType from './Pages/SelectType';
+import UploadImg from './Pages/UploadImg';
+import NewPattern from './Pages/NewPattern';
+import ShowPattern from './Pages/ShowPattern';
+import AdditionalInfo from './Pages/AdditionalInfo';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  // const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="NewPattern" component={NewPattern} options={{ headerShown: false, }} />
+        <Stack.Screen name="SelectType" component={SelectType} options={{ headerShown: false, }} />
+        <Stack.Screen name="UploadImg" component={UploadImg} options={{ headerShown: false, }} />
+        <Stack.Screen name="ShowPattern" component={ShowPattern} options={{ headerShown: false, }} />
+        <Stack.Screen name="AdditionalInfo" component={AdditionalInfo} options={{ headerShown: false, }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
